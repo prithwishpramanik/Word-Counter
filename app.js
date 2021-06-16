007e7a6f55dbe8282b5a9969bd426e03666ca970
Prithwish Pramanik <prithwishpramanik462@gmail.com>
2021-06-16 20:58:46 +0530

    V1.0
    V1.0

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
-
-
-
-
 document.getElementById("button").addEventListener("click", function(event){
     let words = document.getElementById("exampleFormControlTextarea1").value;
-    var x=words.length;
+    words.replace(/(^\s*)|(\s*$)/gi,"");
+    words.replace(/[ ]{2,}/gi," ");
+    words.replace(/\n /,"\n");
+
+    var y = (words.match(/[a-z]/gi) || []).length;
+    let x=words.split(" ").length;
     console.log(x);
+    console.log(y);
     console.log(words);
+    document.getElementById('exampleFormControlTextarea1').value = '';
+    document.getElementById('word').innerHTML = x;
+    document.getElementById('letters').innerHTML = y;
+
+
 
     event.preventDefault();
 })
